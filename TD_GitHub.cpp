5d7bcab1d5b956cc40700c726c3cc16290a8ae6d
vivele69 <128789131+vivele69@users.noreply.github.com>
2023-04-07 09:01:17 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,31 +1,4 @@
-//Fonctions réalisées par l'étudiant 1(Mathis Pignard ) :
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Exemples d'utilisation des fonctions
-    int age = 20;
-    if (estMajeur(age)) {
-        cout << "La personne est majeure" << endl;
-    } else {
-        cout << "La personne est mineure" << endl;
-    }
-
-    double x = 2.5;
-    int n = 3;
-    cout << "x^n = " << exposant(x, n) << endl;
-
-    double prix = 50.0;
-    cout << "Prix avec TVA : " << TVA(prix) << endl;
-
-    int a = 10, b = 20, c = 30;
-    cout << "Le plus grand des trois nombres est : " << plusGrand(a, b, c) << endl;
-
-    return 0;
-}
-
-
-
+//Fonctions réalisées par l'étudiant 1(Mathis Pignard ) 
 
 bool estMajeur(int age) {
     if (age >= 18) {
@@ -35,17 +8,17 @@ bool estMajeur(int age) {
     }
 }
 
-double exposant(double x, int n) {
-    double result = 1.0;
+float exposant(float x, int n) {
+    float result = 1.0;
     for (int i = 0; i < n; i++) {
         result *= x;
     }
     return result;
 }
 
-double TVA(double prix) {
-    double tva = 0.196;
-    return prix * (1 + tva);
+    float TVA(float prix) {
+    float TVA = 0.196;
+    return prix * (1 + TVA);
 }
 
 int plusGrand(int a, int b, int c) {
